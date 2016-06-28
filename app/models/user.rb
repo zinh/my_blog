c4620edfc366397a393854559f1f804af727f4e8
Vinh Nguyen <xuanvinhct@gmail.com>
2016-06-28 09:03:31 +0700

    user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+  validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
+
+  def self.authenticate(name, password)
+    User.find_by(name: name, password: password)
+  end
 end
